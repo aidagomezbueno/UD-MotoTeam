928355452bef48dbcd0bd65668aa27be1915fa9c
Aida Gomezbueno Berezo <71767272+aidagomezbueno@users.noreply.github.com>
2021-12-03 17:38:48 +0100

    Favicon
    Funciona favicon - dentro de static.

--- udmototeamapp/urls.py
+++ udmototeamapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+# from django.contrib.staticfiles.storage import staticfiles_storage
+# from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    # path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
 ]
