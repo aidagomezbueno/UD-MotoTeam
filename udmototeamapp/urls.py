367e95f8e12cc265c7a38cb446f0df059c73c1f1
Aida Gomezbueno Berezo <71767272+aidagomezbueno@users.noreply.github.com>
2022-03-29 22:08:03 +0200

    29032022
    Nueva plantilla.

--- udmototeamapp/urls.py
+++ udmototeamapp/urls.py
@@ -4,6 +4,11 @@ from . import views
 # from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
+    path('index/', views.inicio, name='inicio'),
+    path('', views.index, name='index'),
+    path('products/', views.products, name='products'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
     # path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
 ]
--- udmototeamapp/views.py
+++ udmototeamapp/views.py
@@ -7,3 +7,18 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'inicio.html')
+
+def index(request):
+    return render(request, 'index.html')
+
+def products(request):
+    return render(request, 'products.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
